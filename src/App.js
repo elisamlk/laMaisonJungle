1fc20c3cb13742117114ce9aa0ce4993c45cca61
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-28 18:51:37 +0200

    Mise en place du banner et du style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// import logo from './logo.svg';
-// import './App.css';
-
+import "./App.css";
 import Cart from "./components/Cart";
 import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
-      <Banner></Banner>
-      <Cart></Cart>
+      <Banner />
+      <Cart />
     </div>
   );
 }
