c1b0dc3de309a08c816026f77759d1966aae0206
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-28 17:19:03 +0200

    Création du composant Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
+import "./components/Header"
+import Header from "./components/Header";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-function Header() {
-  return <h1>La maison jungle</h1>;
+function App() {
+  return (
+    <div className="App">
+    <Header></Header>
+    </div>
+  );
 }
 
-export default Header;
+
+export default App;
+
+
+
+
