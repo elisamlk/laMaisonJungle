73e3b1bd9284160938d04f202c61489125a823b1
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-28 17:37:44 +0200

    Création du composant Cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
-import "./components/Header"
+
 import Header from "./components/Header";
+import Description from "./components/Description";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div className="App">
-    <Header></Header>
+      <Header></Header>
+      <Description></Description>
+      <Cart></Cart>
     </div>
   );
 }
 
-
 export default App;
-
-
-
-
