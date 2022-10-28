9702fd1f1b450591dc1aa4da1311a8cd5932a76c
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-10-28 19:46:45 +0200

    Ajout du composant ShoppingList et de la fonction map pour afficher les différentes plantes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Cart from "./components/Cart";
 import Banner from "./components/Banner";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   );
 }
